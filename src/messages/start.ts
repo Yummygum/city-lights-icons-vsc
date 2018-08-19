303a97fcac7639a86a91ae067bce27f68e0b988b
Javier Diaz <coderdiaz@gmail.com>
2018-08-19 17:02:16 -0500

    added start message

---
+++ src/messages/start.ts
@@ -0,0 +1,14 @@
+import * as vscode from 'vscode';
+import { ThemeStatus } from '../helpers/versioning';
+
+/** Initialization of the icons every time the theme get activated */
+export const showStartMessages = (themeStatus: Promise<ThemeStatus>) => {
+    return themeStatus.then((status: ThemeStatus) => {
+        if (status === ThemeStatus.updated) {
+          console.log('UPDATED');
+        }
+        else if (status === ThemeStatus.neverUsedBefore) {
+          vscode.window.showInformationMessage('Thanks for use City Lights Themes');
+        }
+    });
+};
